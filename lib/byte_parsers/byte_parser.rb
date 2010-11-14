20e873ae9d8e1e651f83b08936883f948bca3b60
Michael Edgar <michael.j.edgar@dartmouth.edu>
2010-11-13 20:51:20 -0600

    Added specs for the ByteParser subclasses and added basic reading. No laziness, just reads each field in a row.

--- lib/byte_parsers/byte_parser.rb
+++ lib/byte_parsers/byte_parser.rb
@@ -1,9 +1,15 @@
 class ByteParser
-  Field = Struct.new(:name, :parser, :opts)
+  Field = Struct.new(:name, :parser)
+  
+  class << self
+    attr_accessor :result_class
+  end
   
   def self.new(&blk)
     result = Class.new(ByteParser)
+    result.result_class = Class.new
     result.instance_eval(&blk)
+    result.add_methods
     result
   end
   
@@ -12,6 +18,21 @@ class ByteParser
   end
   
   def self.add(name, parser, opts={})
-    self.fields << Field.new(name, parser, opts)
+    self.fields << Field.new(name, parser.new(opts))
+  end
+  
+  def self.read(input)
+    result = self.result_class.new
+    self.fields.each do |field|
+      value = field.parser.read(input)
+      result.__send__(:instance_variable_set, "@#{field.name}".to_sym, value)
+    end
+    result
+  end
+  
+  def self.add_methods
+    self.fields.each do |field|
+      self.result_class.send(:attr_reader, field.name.to_sym)
+    end
   end
 end
--- spec/byte_parsers_spec.rb
+++ spec/byte_parsers_spec.rb
@@ -1,10 +1,10 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "ByteParsers" do
-  before do
+  before(:all) do
     SimpleParser = ByteParser.new do
       add :fourcc, BP::UInt32, :endian => :big
-      add :version, BP::UInt16, :endian => :big
+      add :version, BP::UInt16, :endian => :little
       add :name, BP::CString
       add :tag, BP::FixedString, :size => 7
       add :weird_string, BP::String, :terminator => "4"
@@ -14,5 +14,33 @@ describe "ByteParsers" do
 
   it "allows declarative class creation" do
     SimpleParser.should be_a(Class)
+  end
+
+  it 'has a #read method that reads in an IO' do
+    SimpleParser.should respond_to(:read)
+  end
+  
+  describe '#read' do
+    it 'returns an object with readers for each field' do
+      input = "\x01\x02\x03\x04ABMichael\0MJEMJEMThisEndsWith4EndsWith3okay"
+      result = SimpleParser.read(StringIO.new(input))
+      result.should respond_to(:fourcc)
+      result.should respond_to(:version)
+      result.should respond_to(:name)
+      result.should respond_to(:tag)
+      result.should respond_to(:weird_string)
+      result.should respond_to(:number_terminated_string)
+    end
+    
+    it 'parses basic input' do
+      input = "\x01\x02\x03\x04ABMichael\0MJEMJEMThisEndsWith4EndsWith3okay"
+      result = SimpleParser.read(StringIO.new(input))
+      result.fourcc.should == 0x01020304
+      result.version.should == 0x4241
+      result.name.should == 'Michael'
+      result.tag.should == 'MJEMJEM'
+      result.weird_string.should == 'ThisEndsWith'
+      result.number_terminated_string.should == 'EndsWith'
+    end
   end
 end
